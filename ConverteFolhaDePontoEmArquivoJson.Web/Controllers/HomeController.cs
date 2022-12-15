0ed0ce65f0be881b6347c92b10d21800c6765d26
Aline-Stephanie <alinestephaniefreitas@gmail.com>
2022-12-15 01:44:49 -0300

    Valida Dados

--- ConverteFolhaDePontoEmArquivoJson.Web/Controllers/HomeController.cs
+++ ConverteFolhaDePontoEmArquivoJson.Web/Controllers/HomeController.cs
@@ -2,61 +2,60 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace ConverteFolhaDePontoEmArquivoJson.Web.Controllers
+namespace ConverteFolhaDePontoEmArquivoJson.Web.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        [HttpGet]
-        public IActionResult Index()
+    [HttpPost]
+    public async Task<IActionResult> Index(IFormFile userfile)
+    {
+        try
         {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Index(IFormFile userfile)
-        {
-            try
+            string filename = userfile.FileName;
+            if (filename.Contains(".csv"))
             {
-                string filename = userfile.FileName;
-                if (filename.Contains(".csv"))
-                {
-                    filename = Path.GetFileName(filename);
-                    string uploadfilepath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", filename);
-                    using var stream = new FileStream(uploadfilepath, FileMode.Create);
-                    await userfile.CopyToAsync(stream);
-                    ViewBag.message = "Upload concluído";
-                    stream.Close();
-                    Repository.Conversor.GeraJson(uploadfilepath);
-                }
-                else ViewBag.message = "Tipo de arquivo não suportado. Insira um arquivo .csv";
+                filename = Path.GetFileName(filename);
+                string uploadfilepath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", filename);
+                using var stream = new FileStream(uploadfilepath, FileMode.Create);
+                await userfile.CopyToAsync(stream);
+                ViewBag.message = "Upload concluído";
+                stream.Close();
+                Repository.Conversor.GeraJson(uploadfilepath);
             }
-            catch (Exception ex)
-            {
-                ViewBag.message = "Error" + ex.Message.ToString();
-            }
-            return View();
+            else ViewBag.message = "Tipo de arquivo não suportado. Insira um arquivo .csv";
         }
+        catch (Exception ex)
+        {
+            ViewBag.message = "Error" + ex.Message.ToString();
+        }
+        return View();
+    }
 
-        public IActionResult About()
-        {
-            return View();
-        }
-        public IActionResult Home()
-        {
-            return View();
-        }
+    public IActionResult About()
+    {
+        return View();
+    }
+    public IActionResult Home()
+    {
+        return View();
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
--- Repository/Conversor.cs
+++ Repository/Conversor.cs
@@ -9,8 +9,12 @@ namespace Repository;
 
 public class Conversor
 {
-
     public static void GeraJson(string caminho)
+    {
+        ObtemDadosDoCsv(caminho);
+        
+    }
+    public static void ObtemDadosDoCsv(string caminho)
     {
         int i;
         var columnCodigo = new List<string>();
@@ -22,32 +26,51 @@ public class Conversor
         var columnAlmoco = new List<string>();
         var registrosDePonto = new List<RegistroDePonto>();
 
-        using (StreamReader reader = new StreamReader(caminho))
+        using StreamReader reader = new StreamReader(caminho);
+        reader.Read();
+        do
         {
-            reader.Read();
-            do
-            {
-                string[] splits = reader.ReadLine().Split(';');
-                columnCodigo.Add(splits[0]);
-                columnFuncionario.Add(splits[1]);
-                columnValorHora.Add(splits[2]);
-                columnData.Add(splits[3]);
-                columnEntrada.Add(splits[4]);
-                columnSaida.Add(splits[5]);
-                columnAlmoco.Add(splits[6]);
+            string[] splits = reader.ReadLine().Split(';');
+            columnCodigo.Add(splits[0]);
+            columnFuncionario.Add(splits[1]);
+            columnValorHora.Add(splits[2]);
+            columnData.Add(splits[3]);
+            columnEntrada.Add(splits[4]);
+            columnSaida.Add(splits[5]);
+            columnAlmoco.Add(splits[6]);
 
-            } while (!reader.EndOfStream);
+        } while (!reader.EndOfStream);
 
-            reader.Close();
+        reader.Close();
 
-            for (i=1; i < columnCodigo.Count; i++)
+        for (i = 1; i < columnCodigo.Count; i++)
+        {
+            try
             {
                 RegistroDePonto registro = new RegistroDePonto(columnCodigo[i], columnFuncionario[i], columnValorHora[i], columnData[i], columnEntrada[i], columnSaida[i], columnAlmoco[i]);
+                ValidaDados(registro);
                 registrosDePonto.Add(registro);
-
                 Console.WriteLine(registro.ToString());
-
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+    public static void ValidaDados(RegistroDePonto registro)
+    {
+        try
+        {
+            int a = int.Parse(registro.CodigoDoFuncionario);
+            decimal c = decimal.Parse(registro.ValorHora);
+            DateOnly d = DateOnly.Parse(registro.Data);
+            TimeSpan e = TimeSpan.Parse(registro.Entrada);
+            TimeSpan f = TimeSpan.Parse(registro.Saida);
+
+        }
+        catch{
+            Console.WriteLine("Dados Inválidos");
         }
     }
 }
